b8aeb2ca72b2cadab25d44b596603f927eec1a82
unknown <jazzstephens2015@gmail.com>
2019-06-19 00:20:45 -0500

    updated app.py code

--- StarterCode/app.py
+++ StarterCode/app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["Chicago_neighborhoods_db"] = DATABASE_URL
 db = SQLAlchemy(app)
 
 # # reflect an existing database into a new model
@@ -42,15 +42,68 @@ def index():
 
 
 # Routes for data
-
-@app.route("/api/haunted")
-def getHauntedData():
+#asian restaurant data
+@app.route("/api/asian")
+def getAsianData():
     df = pd.read_sql("""
         select  * 
-        from    haunted_table
+        from    asian_table
         """, db.engine)
-    csv_file = df.to_csv()
-    return csv_file
-
+    asian_file = df.to_csv()
+    return asian_file
+#coffe data
+@app.route("/api/coffee")
+def getCoffeeData():
+    df = pd.read_sql("""
+        select  * 
+        from    coffee_table
+        """, db.engine)
+    coffee_file = df.to_csv()
+    return coffee_file
+#ghost data
+@app.route("/api/ghost")
+def getGhostData():
+    df = pd.read_sql("""
+        select  * 
+        from    ghost_table
+        """, db.engine)
+    ghost_file = df.to_csv()
+    return ghost_file
+#library data
+@app.route("/api/library")
+def getLibraryData():
+    df = pd.read_sql("""
+        select  * 
+        from    library_table
+        """, db.engine)
+    library_file = df.to_csv()
+    return library_file
+#Mexican data
+@app.route("/api/mexican")
+def getMexicanData():
+    df = pd.read_sql("""
+        select  * 
+        from    mexican_table
+        """, db.engine)
+    mexican_file = df.to_csv()
+    return mexican_file
+#pizza data
+@app.route("/api/pizza")
+def getPizzaData():
+    df = pd.read_sql("""
+        select  * 
+        from    pizza_table
+        """, db.engine)
+    pizza_file = df.to_csv()
+    return pizza_file
+#redlight data
+@app.route("/api/redlight")
+def getRedlightData():
+    df = pd.read_sql("""
+        select  * 
+        from    redlight_table
+        """, db.engine)
+    redlight_file = df.to_csv()
+    return redlight_file
 if __name__ == "__main__":
     app.run()

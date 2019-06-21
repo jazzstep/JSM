fc3a58c6ab59035c03a894ad5e83e657b8cf9f63
unknown <jazzstephens2015@gmail.com>
2019-06-20 21:24:17 -0500

    test

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set location of flask app via enviroment variable FLASK_APP
-export FLASK_APP=app:app_flask
+export FLASK_APP=app:flask_app
 # Check command line arguments for "dev" flag
 if [[ $1 == "dev" ]]; then
     # Set enviroment variable FLASK_ENV to development (for auto reload)

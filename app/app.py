fc3c3273fa2bc8b162cb67f8fed13dcc3c3dc8d7
unknown <jazzstephens2015@gmail.com>
2019-06-22 15:52:56 -0500

    moving life to app.py

--- app/app.py
+++ app/app.py
@@ -44,6 +44,142 @@ db.init_app(app)
 
 # Routes for webpages
 
+@app.before_first_request
+def loadCsvData():
+    # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "asianUpdate.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "asian_table"
+
+    print(f"Importing csv {'asianUpdate'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    print(df.head())
+
+    print(f"Saving results to {'asian_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+
+    #-----------------------------------------------------------#
+        # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "coffeeUpdate.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "coffee_table"
+
+    print(f"Importing csv {'coffeeUpdate'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    # print(df.head())
+
+    print(f"Saving results to {'coffee_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+    #-----------------------------------------------------------#
+        # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "CPLdata1.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "library_data"
+
+    print(f"Importing csv {'CPLdata1'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    # print(df.head())
+
+    print(f"Saving results to {'library_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+        #-----------------------------------------------------------#
+        # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "hauntedUpdate.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "haunted_data"
+
+    print(f"Importing csv {'haunted'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    # print(df.head())
+
+    print(f"Saving results to {'haunted_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+            #-----------------------------------------------------------#
+        # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "mexicanUpdate.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "mexican_data"
+
+    print(f"Importing csv {'mexican'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    # print(df.head())
+
+    print(f"Saving results to {'mexican_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+                #-----------------------------------------------------------#
+        # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "pizzaUpdate.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "pizza_data"
+
+    print(f"Importing csv {'pizza'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    # print(df.head())
+
+    print(f"Saving results to {'pizza_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+                    #-----------------------------------------------------------#
+        # Import a csv file into a pandas dataframe, clean, then save the results to a database table
+    path_for_this_file = os.path.dirname(__file__)
+    file_name = "red-light-camera-locations.csv"
+    absolute_csv_path = os.path.join(path_for_this_file, "Datasets", file_name)
+    db_table_name = "redlight_data"
+
+    print(f"Importing csv {'redlight'}")
+    df = pd.read_csv(absolute_csv_path)
+
+    # print("\nClean and transform ....\n")
+    # # CLEAN
+    # # TRANSFORM
+    # print(df.head())
+
+    print(f"Saving results to {'redlight_data'}")
+    # Save results from import as 
+    df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
+    print("DONE!")
+
 @app.route("/")
 def renderHome():
     return render_template("index.html")
@@ -53,13 +189,13 @@ def renderHome():
 @app.route("/api/asian")
 def getAsianData():
     df = pd.read_sql("""
-    select  * from    asian_data""", db.engine)
+    select  * from    asian_table""", db.engine)
     asian_file = df.to_csv()
     return asian_file
 #coffe data
 @app.route("/api/coffee")
 def getCoffeeData():
-    df = pd.read_sql("""select  * from    coffee_data""", db.engine)
+    df = pd.read_sql("""select  * from    coffee_table""", db.engine)
     coffee_file = df.to_csv()
     return coffee_file
 #ghost data
